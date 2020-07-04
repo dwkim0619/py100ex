65411163d92d3d12cf8ddea36e23eaa3541269b8
kim, dae-wook <dwkim0619@gmail.com>
2020-07-04 11:33:53 +0900

    .

---
+++ 004.BeautifulSoup 객체 만들기.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/Seoul_Metropolitan_Subway"
+resp = requests.get(url)
+html_src = resp.text
+
+soup = BeautifulSoup(html_src, "html.parser")
+print(type(soup))
+
+print(soup.head)
+print(soup.body)
+
+print("title tag : ", soup.title)
+print("title tag name : ", soup.title.name)
+print("title tag string : ", soup.title.string)
